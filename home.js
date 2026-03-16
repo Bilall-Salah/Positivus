25a7963748c861eee19a49f824d7a94c23f1c9ad
Bilal Salah <bilal.salah.mohameed@gmail.com>
2026-03-16 04:12:48 +0200

    update project design

--- home.js
+++ home.js
@@ -1,4 +1,14 @@
 let plus = document.querySelectorAll(".plus");
+let slider = document.getElementById("slider");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let dots = document.querySelectorAll(".dot");
+let dotIndex = 0;
+let width = 0;
+let maxwidth = 225;
+let sendMessage = document.getElementById("sendMessage");
+let hiChecked = document.getElementById("hiChecked");
+let getChecked = document.getElementById("getChecked");
 plus.forEach((btn) => {
     btn.addEventListener("click", () => {
         const main = btn.closest(".main");
@@ -10,14 +20,6 @@ plus.forEach((btn) => {
 
     });
 });
-
-let slider = document.getElementById("slider");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-let dots = document.querySelectorAll(".dot");
-let dotIndex = 0;
-let width = 0;
-let maxwidth = 225;
  function activeDot() {
      dots.forEach((dot) => {
          dot.classList.remove("active"); 
@@ -40,4 +42,59 @@ prev.addEventListener("click", () => {
         activeDot();
     }
 });
+sendMessage.addEventListener("click", (e) => {
+    let hiChecked = document.getElementById("hiChecked");
+    let getChecked = document.getElementById("getChecked")
+    let choose = document.getElementById("choose");
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let message = document.getElementById("message");
+    let nameError = document.getElementById("nameError");
+    let emailError = document.getElementById("emailError");
+    let messageError = document.getElementById("messageError");
+    if (!hiChecked.checked && !getChecked.checked) {
+        choose.classList.remove("hidden");
+        e.preventDefault();
+    } else {
+        choose.classList.add("hidden");
+    }
+    if (name.value.trim() === "") {
+        e.preventDefault();
+        nameError.classList.remove("hidden");   
+    } else {
+        nameError.classList.add("hidden");  
+    }
+    if ( !email.value.includes("@")) {
+        e.preventDefault();
+        emailError.classList.remove("hidden");   
+    } else {
+        emailError.classList.add("hidden");  
+    }
+    if (message.value.trim() === "") {
+        e.preventDefault();
+        messageError.classList.remove("hidden");   
+    } else {
+        messageError.classList.add("hidden");  
+    }
+    if (hiChecked.checked || getChecked.checked && name.value.trim() !== "" && email.value.includes("@") && message.value.trim() !== "") { 
+        alert("Message sent successfully!");
+    }
+});
 
+hiChecked.addEventListener("change", () => {
+    if (hiChecked.checked) {
+        getChecked.checked = false;
+        getChecked.disabled = true;
+    } else {
+        getChecked.disabled = false;
+    }
+});
+
+getChecked.addEventListener("change", () => {
+    if (getChecked.checked) {
+        hiChecked.checked = false;
+        hiChecked.disabled = true;
+    } else {
+        hiChecked.disabled = false;
+    }
+});
